8a5e27d859504cc1b4181669039af8fe34b6ef88
lizejia <376489098@qq.com>
2017-01-10 19:14:43 +0800

    迭代器
    C#1.0中实现迭代器，C#2.0用yield关键字实现迭代器，

---
+++ DomeMvc/DomeIterator/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomeIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Friends fs = new Friends();
+            foreach (Friend item in fs)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadKey();
+        }
+
+
+        public class Friend 
+        {
+            public string Name { get; set; }
+            public Friend(string name)
+            {
+                this.Name = name;
+            }
+        }
+
+        public class Friends : IEnumerable
+        {
+            private Friend[] friendarray;
+            public Friends()
+            {
+                friendarray = new Friend[]
+                {
+                    new Friend("张三"),
+                    new Friend("李四"),
+                    new Friend("王麻子")
+                };
+            }
+            /// <summary>
+            /// 索引器
+            /// </summary>
+            /// <param name="index"></param>
+            /// <returns></returns>
+            public Friend this[int index]
+            {
+                get { return friendarray[index]; }
+            }
+
+            public int Count
+            {
+                get { return friendarray.Length; }
+            }
+
+            /// <summary>
+            /// 实现IEnumerable接口方法
+            /// 要获得迭代器必须实现IEnumerable.GetEnumerator()方法
+            /// </summary>
+            /// <returns></returns>
+            public IEnumerator GetEnumerator()
+            {
+                //在C#1.0中实现一个迭代器
+                //return new FriendIterator(this);
+
+                //在C#2.0中实现一个迭代器
+                for (int i = 0; i < friendarray.Length; i++)
+                {
+                    yield return friendarray[i];
+                }
+                
+            }
+        }
+
+        /// <summary>
+        /// 在C#1.0中实现一个迭代器
+        /// 必须实现IEnumerator的Current()、Reset()和MoveNext()
+        /// </summary>
+        public class FriendIterator : IEnumerator
+        {
+            private readonly Friends friends;
+            private int index;
+            private Friend current;
+
+            public FriendIterator(Friends _friends)
+            {
+                this.friends = _friends;
+                this.index = 0;
+            }
+
+
+            public object Current
+            {
+                get { return this.current; }
+            }
+
+            public bool MoveNext()
+            {
+                if (index + 1 > friends.Count)
+                {
+                    return false;
+                }
+                else
+                {
+                    this.current = friends[index];
+                    index++;
+                    return true;
+                }
+            }
+
+            public void Reset()
+            {
+                index = 0;
+            }
+        }
+
+    }
+}
